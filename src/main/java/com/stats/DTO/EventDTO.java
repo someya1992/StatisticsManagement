1182ff47a4a041465d9a5be8ec6619a7c3dffc48
Someya Goel <someyagoel@gmail.com>
2021-01-10 17:04:30 +0100

    Add Exception and Validation Handling

--- src/main/java/com/stats/DTO/EventDTO.java
+++ src/main/java/com/stats/DTO/EventDTO.java
@@ -5,14 +5,19 @@ import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @AllArgsConstructor
 public class EventDTO {
 
+	@NotNull(message = "TimeStamp is required")
 	private Long timeStamp;
 
+	@NotNull(message = "x is required")
 	private BigDecimal x;
 
+	@NotNull(message = "y is required")
 	private int y;
 
 }
